9418833e3873effef4ddbdf1ff002decdda15e0f
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-08-22 11:11:32 +0900

    다형성(정적 바인딩)

---
+++ 05-03 다형성.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+public:
+	void walk(void)
+	{
+		cout << "걷다" << endl;
+	}
+
+	void bark(void)
+	{
+		cout << "짖다" << endl;
+	}
+
+	void eat(void)
+	{
+		cout << "먹다" << endl;
+	}
+
+	Animal(string name, unsigned int age, int leg_num)
+		: name_(name), age_(age), leg_num_(leg_num) {}
+
+private:
+	string name_;
+	unsigned int age_;
+	int leg_num_;
+};
+
+class Dog : public Animal {
+public:
+	Dog (string name, unsigned int age, int leg_num, int loyalty)
+		: Animal(name, age, leg_num), loyalty_(loyalty) {
+		cout << "충성도" << endl;
+	}
+	//C++은 디폴트가 정적바인딩이기 때문에 가상함수로 오버라이딩 해야 한다
+	void bark() {
+		cout << "울프웊프" << endl;
+	}
+	void eat() {
+		cout << "왕왕" << endl;
+	}
+	void walk() {
+		cout << "척척촨촵" << endl;
+	}
+
+private:
+	int loyalty_;
+};
+
+void main(void)
+{
+	Animal* animal = new Animal("요아정", 8, 2);
+	animal->bark();
+	animal->eat();
+	animal->walk();
+	delete animal;
+
+	animal = new Dog("마루", 5, 2, 100);
+	animal->bark();
+	animal->eat();
+	animal->walk();
+	delete animal;
+}
