0f34435671c68b63eb4fd0b3aab880203f86f631
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-09-24 15:49:21 +0900

    08-02 맵

---
+++ 08-01 맵.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+void main(void) {
+	map < string, long long > money;
+
+	//¸Ê Ãß°¡
+	money["Áö¼ö"] = 10000000000;
+	money["¼öºó"] = 30000000000;
+	money["¹Î¼±"] = 100;
+
+	//¹Ýº¹ÀÚ
+	map<string, long long>::iterator iter;
+	for (iter = money.begin(); iter != money.end(); iter++)
+		cout << iter->first << "´Â " << iter->second << endl;
+
+	//°ª ¼öÁ¤
+	money["¹Î¼±"] = 10000000000;
+	cout << money["¹Î¼±"] << endl;
+
+	//Å©±â
+	cout << money.size() << endl;
+
+}
