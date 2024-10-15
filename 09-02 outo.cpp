0865e7264b062f1aab311f5ca2dd369f00a489a3
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-10-15 14:39:13 +0900

    09-02 auto

---
+++ 09-02 outo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+void main(void) {
+	vector<string> v = { "롯데관광", "동화면세점", "제주드림타워", "마이데일리" };
+
+	for (auto str : v)
+		cout << str << endl;
+
+	auto a = 30;
+}
